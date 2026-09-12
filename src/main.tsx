3f02468b642ed163285de3c024152b5c5171031b
Magic <185496821+Mjc-g3@users.noreply.github.com>
2026-09-12 15:04:37 +0200

    Use GitHub storage messages in LinkVault

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { initGitHubSyncUI } from './github-sync-ui'
+import { initStorageMessageFix } from './storage-message-fix'
+
+initStorageMessageFix()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
